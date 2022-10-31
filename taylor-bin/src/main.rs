07f64481e74c1f7637e02116adc923e592ce2bef
Brent Westbrook <brentrwestbrook@gmail.com>
2022-10-31 15:27:46 -0400

    add some sweet clap stuff

--- taylor-bin/src/main.rs
+++ taylor-bin/src/main.rs
@@ -1,21 +1,34 @@
+use clap::Parser;
 use intder::Intder;
 use symm::{Atom, Molecule};
 use taylor::Taylor;
 
-// both of these can probably be clap flags
-const SYMM_EPS: f64 = 1e-6;
-const STEP_SIZE: f64 = 0.005;
+/// generate intder and anpass input files with a taylor series expansion
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// intder input file header with internal coordinates and geometry
+    #[arg(value_parser)]
+    infile: String,
+
+    /// write intder and anpass input files using the SICs
+    #[arg(short, long, default_value_t = false)]
+    write: bool,
+
+    /// tolerance to use for symmetry equivalence
+    #[arg(short, long, default_value_t = 1e-6)]
+    eps: f64,
+
+    /// step size
+    #[arg(short, long, default_value_t = 0.005)]
+    step_size: f64,
+}
 
 // this is pieced together from parts of pbqff, but it's not clear how to reuse
 // any of the parts
 fn main() -> std::io::Result<()> {
-    let infile = if let Some(infile) = std::env::args().nth(1) {
-        infile
-    } else {
-        eprintln!("usage: taylor INFILE");
-        return Err(std::io::Error::from(std::io::ErrorKind::NotFound));
-    };
-    let mut intder = Intder::load_file(&infile);
+    let cfg = Args::parse();
+    let mut intder = Intder::load_file(&cfg.infile);
     let pairs = intder.geom.0.iter().zip(&intder.atoms);
     let mut atoms = Vec::new();
     for (g, a) in pairs {
@@ -26,7 +39,7 @@ fn main() -> std::io::Result<()> {
         mol.normalize();
         mol
     };
-    let pg = mol.point_group_approx(SYMM_EPS);
+    let pg = mol.point_group_approx(cfg.eps);
 
     println!("Normalized Geometry:\n{:20.12}", mol);
     println!("Point Group = {}", pg);
@@ -36,7 +49,7 @@ fn main() -> std::io::Result<()> {
     let mut disps = Vec::new();
     for i in 0..nsic {
         let mut disp = vec![0.0; nsic];
-        disp[i] = STEP_SIZE;
+        disp[i] = cfg.step_size;
         disps.push(disp);
     }
     intder.disps = disps;
@@ -48,7 +61,7 @@ fn main() -> std::io::Result<()> {
     for (i, disp) in disps.iter().enumerate() {
         let disp = disp.as_slice();
         let m = Molecule::from_slices(atomic_numbers.clone(), disp);
-        let irrep = match m.irrep_approx(&pg, SYMM_EPS) {
+        let irrep = match m.irrep_approx(&pg, cfg.eps) {
             Ok(rep) => rep,
             Err(e) => panic!("failed on coord {} with {}", i, e.msg()),
         };
@@ -68,25 +81,27 @@ fn main() -> std::io::Result<()> {
     println!("\nSymmetry Internal Coordinates:");
     intder.print_sics(&mut std::io::stdout(), &just_irreps);
 
-    // generate checks
-    let checks = Taylor::make_checks(irreps, &pg);
-    // run taylor.py to get fcs and disps
-    let taylor = Taylor::new(5, nsic, checks.0, checks.1);
-    let taylor_disps = taylor.disps();
+    if cfg.write {
+        // generate checks
+        let checks = Taylor::make_checks(irreps, &pg);
+        // run taylor.py to get fcs and disps
+        let taylor = Taylor::new(5, nsic, checks.0, checks.1);
+        let taylor_disps = taylor.disps();
 
-    intder.disps = taylor_disps.to_intder(STEP_SIZE);
+        intder.disps = taylor_disps.to_intder(cfg.step_size);
 
-    let mut f = std::fs::File::create("intder.in")?;
-    use std::io::Write;
-    writeln!(f, "{}", intder)?;
+        let mut f = std::fs::File::create("intder.in")?;
+        use std::io::Write;
+        writeln!(f, "{}", intder)?;
 
-    let anpass = taylor.to_anpass(
-        &taylor_disps,
-        &vec![0.0; taylor_disps.len()],
-        STEP_SIZE,
-    );
-    let mut f = std::fs::File::create("anpass.in")?;
-    writeln!(f, "{}", anpass)?;
+        let anpass = taylor.to_anpass(
+            &taylor_disps,
+            &vec![0.0; taylor_disps.len()],
+            cfg.step_size,
+        );
+        let mut f = std::fs::File::create("anpass.in")?;
+        writeln!(f, "{}", anpass)?;
+    }
 
     Ok(())
 }
